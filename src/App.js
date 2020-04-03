91d68916dd5e99c60a763282864c25b005c1da9d
Matheus Martins de Sales - MSE <matheus.sales@iteris.com.br>
2020-04-02 22:33:15 -0300

    test: show pass test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.svg';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Angular
         </a>
       </header>
     </div>
